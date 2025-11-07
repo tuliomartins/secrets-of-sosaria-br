ff8c2027a0d28dd2e3c24e139d05459841679bff
Tulio Marcos Miranda Martins <tulio_mmiranda@hotmail.com>
2025-11-07 05:21:45 -0800

    Add warglave

---
+++ Data/Scripts/Items/Magical/Artifacts/Main/Artifact_WarglaiveOfAzzinoth.cs
@@ -0,0 +1,44 @@
+using System;
+using Server;
+
+namespace Server.Items
+{
+	public class Artifact_WarglaiveOfAzzinoth : GiftCrescentBlade
+	{
+		public override int InitMinHits{ get{ return 80; } }
+		public override int InitMaxHits{ get{ return 160; } }
+
+		public override bool CanFortify{ get{ return false; } }
+
+		[Constructable]
+		public Artifact_WarglaiveOfAzzinoth()
+		{
+			Name = "Warglaive Of Azzinoth";
+			Hue = 0x117;
+
+			Attributes.SpellChanneling = 1;
+			WeaponAttributes.HitFireball = 100;
+			ArtifactLevel = 2;
+			Server.Misc.Arty.ArtySetup( this, 10, "" );
+		}
+
+		public Artifact_WarglaiveOfAzzinoth( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 );
+		}
+		
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize( reader );
+			ArtifactLevel = 2;
+
+			int version = reader.ReadInt();
+		}
+	}
+}
